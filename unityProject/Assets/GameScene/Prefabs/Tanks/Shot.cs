9bfc0ea5717aa3739c03fc8fa15125affd8b12fe
AndreDaniluk <andredaniluk@gmail.com>
2016-04-05 12:40:23 +0300

    wip

---
+++ unityProject/Assets/GameScene/Prefabs/Tanks/Shot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour
+{
+    [SerializeField] private Transform barrel;
+    [SerializeField] private GameObject shell;
+    [SerializeField] private float shotPower = 40f;
+
+    private GameObject shellInstance;
+    private Quaternion barrelRotation;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Fire
+            barrelRotation = Quaternion.Euler(barrel.TransformDirection(barrel.eulerAngles));
+            shellInstance = Instantiate(shell, barrel.position, barrelRotation) as GameObject;
+            shellInstance.GetComponent<Rigidbody>().velocity = barrel.TransformDirection(Vector3.forward * shotPower);
+        }
+	}
+}
---
+++ unityProject/Assets/Test/testMenuScene/TestMenuScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using GlobalElement;
+using UnityEngine.SceneManagement;
+
+public class TestMenuScene : MonoBehaviour
+{
+    //тестовый генератор установок пользователя
+
+    public void TestPlay()
+    {
+        GlobalManager globalManager = GlobalManager.Instance;
+        globalManager._PlayerData.GameType = 0;
+        globalManager._PlayerData.PlanetID = 0;
+        globalManager._PlayerData.TanksCount = 1;
+        SceneManager.LoadSceneAsync("game");
+    }
+
+}
